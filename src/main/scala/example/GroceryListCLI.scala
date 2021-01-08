f486467e067b26d7d1a3af043d0c83bab21b21d9
christian-palko <76970812+christian-palko@users.noreply.github.com>
2021-01-07 20:34:26 -0800

    More refactoring. Implemented a while loop to reduce same function-calls

--- src/main/scala/example/GroceryListCLI.scala
+++ src/main/scala/example/GroceryListCLI.scala
@@ -7,32 +7,39 @@ import scala.io.Source
 class GroceryListCLI{
     var itemNamesArray = ArrayBuffer[String]("JALAPENOS", "ONIONS", "CARROTS")
     def useList() {
-        val input = readLine("What would you like to do? \n" +
-        "Type \"1\" to ADD an item\nType \"2\" to CHANGE an item\nType \"3\" to DELETE an item\nType \"4\" to EXIT\n--")
+        var continueUsingList = true
+        while (continueUsingList) {
+            printList()
+            var add, change1, change2, delete = ""
+            val input = readLine("What would you like to do? \n" +
+            "Type \"1\" to ADD an item\nType \"2\" to CHANGE an item\nType \"3\" to DELETE an item\nType \"4\" to EXIT\n--")
 
-        try{
-            input.toInt match {
-                case 1 => var add = readLine("Type the NAME of the item you'd like to add: \n--")
-                    addItem(add.toUpperCase())
-                
-                case 2 => var change1 = readLine("Type the NUMBER of the item you want to change: \n")
-                    if (change1.toInt > 0) {
-                        var change2 = readLine(s"Type the NAME you want to change $change1 to: \n")
-                        changeItem(change1.toInt, change2.toUpperCase())
-                    }
+            try{
+                input.toInt match {
+                    case 1 => add = readLine("Type the NAME of the item you'd like to add: \n--")
+                        addItem(add.toUpperCase())
                     
-                case 3 => var delete = readLine("Type the NAME of the item you'd like to delete: \n")
-                    deleteItem(delete.toUpperCase())
+                    case 2 => change1 = readLine("Type the NUMBER of the item you want to change: \n")
+                        if (change1.toInt > 0 & change1.toInt <= itemNamesArray.length) {
+                            change2 = readLine(s"Type the NAME you want to change $change1 to: \n")
+                            changeItem(change1.toInt, change2.toUpperCase())
+                        }
+                        
+                    case 3 => delete = readLine("Type the NAME of the item you'd like to delete: \n")
+                        deleteItem(delete.toUpperCase())
 
-                case 4 => println("Goodbye!")
+                    case 4 => println("Goodbye!")
+                        continueUsingList = false
+                        
 
-                case _ => println("You did not enter a valid input.\n")
-                    useList()
-            }
-        }   catch {
-                case e: NumberFormatException => println("\nYou did not enter a number.\n")
+                    case _ => println("You did not enter a valid input.\n")
                         useList()
-            }
+                }
+            }   catch {
+                    case e: NumberFormatException => println("\nYou did not enter a number.\n")
+                            useList()
+                }
+        }
     }
 
     def printList() {
@@ -45,21 +52,15 @@ class GroceryListCLI{
     def addItem(item : String) {
         itemNamesArray += item
         println(s"\n❋❋❋ You've added ${item} to your grocery list. ❋❋❋")
-        printList();
-        useList()
     }
 
     def deleteItem(item : String) {
         if (itemNamesArray.contains(item.toUpperCase())) {
             itemNamesArray -= item
             println(s"\n❋❋❋ You've deleted ${item} from your grocery list. ❋❋❋")
-            printList();
-            useList()
         }
         else {
             println("\n --- This is ALREADY NOT on your list. --- ")
-            printList();
-            useList()
         }
     }
 
@@ -68,8 +69,6 @@ class GroceryListCLI{
         if(itemNum <= itemNamesArray.length)  { 
             itemNamesArray(itemNum-1) = item
             println(s"\n❋❋❋ You've changed Item ${itemNum} to ${item} ❋❋❋")
-            printList();
-            useList()
         } else {
             println(s"There are not $itemNum items on your list.")
         }
--- src/main/scala/example/Runner.scala
+++ src/main/scala/example/Runner.scala
@@ -4,6 +4,5 @@ package example
 
 object Runner extends App {
   val glist = new GroceryListCLI()
-  glist.printList()
   glist.useList()
 }
