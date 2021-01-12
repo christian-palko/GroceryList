bee1d2d89c2860813554b25c8a4d9da70b3bbf2b
christian-palko <76970812+christian-palko@users.noreply.github.com>
2021-01-12 09:34:12 -0800

    Finished refactoring functionality out of cases and into reusable functions

--- src/main/scala/example/GroceryListCLI.scala
+++ src/main/scala/example/GroceryListCLI.scala
@@ -2,17 +2,19 @@ package example
 import scala.collection.mutable.ArrayBuffer
 import scala.io.StdIn.readLine
 import scala.io.Source
+import java.io.FileNotFoundException
 
 
 class GroceryListCLI{
-    var itemNamesArray = ArrayBuffer[String]("JALAPENOS", "ONIONS", "CARROTS")
+    var itemNamesArray = ArrayBuffer[String]("JALAPENOS", "ONIONS", "CARROTS") //Starting list before db implementation
+    var continueUsingList = false
+
     def useList() {
-        var continueUsingList = true
+        continueUsingList = true
         while (continueUsingList) {
             printList()
-            var add, change1, change2, delete = ""
             val input = readLine("What would you like to do? \n" +
-            "Type \"1\" to ADD an item" +
+            "\nType \"1\" to ADD an item" +
             "\nType \"2\" to CHANGE an item" +
             "\nType \"3\" to DELETE an item" +
             "\nType \"4\" to IMPORT a list" +
@@ -21,27 +23,15 @@ class GroceryListCLI{
 
             try{
                 input.toInt match {
-                    case 1 => add = readLine("Type the NAME of the item you'd like to add: \n--")
-                        addItem(add.toUpperCase())
-                    
-                    case 2 => change1 = readLine("Type the NUMBER of the item you want to change: \n")
-                        if (change1.toInt > 0 & change1.toInt <= itemNamesArray.length) {
-                            change2 = readLine(s"Type the NAME you want to change $change1 to: \n")
-                            changeItem(change1.toInt, change2.toUpperCase())
-                        }
-                        
+                    case 1 => addItem()
+                    case 2 => changeItem()     
                     case 3 => deleteItem()
-
                     case 4 => appendListWithImport()
-
-                    case 5 => println("Goodbye!")
-                        continueUsingList = false
-                        
-                    case _ => println("You did not enter a valid input.\n")
-                        useList()
+                    case 5 => exitApp()
+                    case _ => println("\n* You did not enter a valid input. *\n")     
                 }
             }   catch {
-                    case e: NumberFormatException => println("\nYou did not enter a number.\n")
+                    case nfe: NumberFormatException => println("\nYou did not enter a number.\n")
                             useList()
                 }
         }
@@ -49,7 +39,9 @@ class GroceryListCLI{
 
     def appendListWithImport () {
         var file = readLine("What is the name of the file you want to import? \n")
-            val openFile = FileUtil.importFile(file)
+            
+        try{
+        val openFile = FileUtil.importFile(file)
             val openFileAdd = openFile
                 .toUpperCase
                 .replaceAll(" ", "")
@@ -59,20 +51,31 @@ class GroceryListCLI{
             println(item)
             itemNamesArray += item
             }
-            println(s"\n You've added: ${openFile.mkString}:")
-                    
+            println(s"\n*** You've added: ${openFile.mkString} ***")
+        } catch {
+            case fnfe: FileNotFoundException => println("\n * This file could not be found. *")
+                var tryAgain = readLine("\nWould you like to try again? YES/NO?\n").toUpperCase()
+                if (tryAgain == "YES" || tryAgain == "Y") {
+                    appendListWithImport()
+                } else if (tryAgain == "NO" || tryAgain == "N") {
+                    useList()
+                } else {
+                    println("\n* That was not a valid input. Going to main menu. *\n")
+                }
+        }          
     }
 
     def printList() {
         println("\nGROCERY LIST:\n────────────────────")
         for (a <- 1 to itemNamesArray.length)
-            println(s"Item $a: ${itemNamesArray(a-1)}")
+            println(s"$a. ${itemNamesArray(a-1)}")
         println("────────────────────\n")
     }
 
-    def addItem(item : String) {
-        itemNamesArray += item
-        println(s"\n❋❋❋ You've added ${item} to your grocery list. ❋❋❋")
+    def addItem() {
+        var add = readLine("Type the NAME of the item you'd like to add: \n--").toUpperCase()
+        itemNamesArray += add
+        println(s"\n❋❋❋ You've added ${add} to your grocery list. ❋❋❋")
     }
 
     def deleteItem() {
@@ -84,20 +87,24 @@ class GroceryListCLI{
             println(s"\n❋❋❋ You've deleted ${delete} from your grocery list. ❋❋❋")
         }
         else {
-            println("\n --- This is ALREADY NOT on your list. --- ")
+            println("\n* This is ALREADY NOT on your list. *")
         }
     }
 
-
-    def changeItem(itemNum : Int, item : String) {
-        
-        if(itemNum <= itemNamesArray.length)  { 
-            itemNamesArray(itemNum-1) = item
-            println(s"\n❋❋❋ You've changed Item ${itemNum} to ${item} ❋❋❋")
-        } else {
-            println(s"There are not $itemNum items on your list.")
-        }
+    def changeItem() {
+        var itemChangeFrom = readLine("\nType the NAME of the item you want to change: \n").toUpperCase()
+            if (itemNamesArray.contains(itemChangeFrom)) {
+                var itemChangeTo = readLine(s"\nType what you'd like to change $itemChangeFrom to: \n").toUpperCase()
+                var indexToChange = itemNamesArray.indexOf(itemChangeFrom)
+                itemNamesArray(indexToChange) = itemChangeTo
+                println(s"\n❋❋❋ You changed $itemChangeFrom to $itemChangeTo. ❋❋❋")
+            } else {
+                println("\n❋ This item does not exist in your list. ❋")
+            }
     }
 
-
+    def exitApp() {
+        println("Goodbye!")
+        continueUsingList = false
+    }
 }
